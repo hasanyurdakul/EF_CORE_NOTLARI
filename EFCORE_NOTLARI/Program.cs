46c8cc96b1c160bcc63f4678dc0fd8a03d068d02
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-04-07 23:04:04 +0300

    Initial commit

---
+++ EFCORE_NOTLARI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCORE_NOTLARI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
