e8794656c0e33555fb8ca406c489590faf89cb95
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-04-08 01:39:23 +0300

    Lesson 9

---
+++ Lesson9/Migrations/20240407223556_mig_1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lesson9.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Urunler",
+                columns: table => new
+                {
+                    UrunID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Urunler", x => x.UrunID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Urunler");
+        }
+    }
+}
---
+++ Lesson9/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Lesson9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public class ETicaretContext : DbContext
+        {
+            public DbSet<Urun> Urunler { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                //Provider 
+                //Connection String
+                //Lazy Loading olacak mı olmayacak mı ?
+                //vb. yapılandırmalar OnConfiguring metodu override edilerek yapılır.
+                optionsBuilder.UseSqlServer("Server=HASAN_YURDAKUL;Database=ETicaretDB;User ID=sa;Password=1q2w3e4r;TrustServerCertificate=True");
+            }
+        }
+        public class Urun
+        {
+            //Aşağıdaki isimlendirmelerin hepsi default konfigürasyonda naming convention nedeni ile Primary Key olarak kabul edilecektir.
+            //public int Id { get; set; }
+            //public int ID { get; set; }
+            //public int UrunId { get; set; }
+            //public int UrunID { get; set; }
+            public int UrunID { get; set; }
+
+        }
+
+
+
+        #region OnConfiguring ile Konfigürasyon Ayarlarını Gerçekleştirmek
+        //EF Core Tool'unu yapılandırmak için kullandığımız bir metottur.
+        //Context nesnesi içinde override edilerek kullanılır.
+
+        #endregion
+
+        #region Basit Düzeyde Entity Tanımlama Kuralları
+        //EF Core, her tablonun varsayılan olarak bir primary key kolonu olmasını gerektiğini kabul eder.
+        //Bundan dolayı, bu kolonu temsil eden bir property tanımlamadığımız taktirde hata verecektir!
+        #endregion
+
+        #region Tablo Adını Belirleme
+        //Default olarak tablo adları, yaratılan "DbSet" nesnelerine verdiğimiz isim olacaktır.
+        #endregion
+
+    }
+}
+
+
+
+
