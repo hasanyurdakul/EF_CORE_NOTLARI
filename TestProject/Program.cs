dc8a158ab0b9977c6220112b81493de3f8a006e5
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-04-07 23:17:26 +0300

    Test2

---
+++ TestProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
