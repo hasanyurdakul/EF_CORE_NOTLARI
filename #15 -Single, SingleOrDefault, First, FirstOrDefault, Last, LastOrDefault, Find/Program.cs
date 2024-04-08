cba03ea3d5de4e4d5eb18415dbee21e6f5c37fa4
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-04-09 01:01:19 +0300

    #15

--- #15 -Single, SingleOrDefault, First, FirstOrDefault, Last, LastOrDefault, Find/Program.cs
+++ #15 -Single, SingleOrDefault, First, FirstOrDefault, Last, LastOrDefault, Find/Program.cs
@@ -4,42 +4,287 @@ namespace _15__Single__SingleOrDefault__First__FirstOrDefault__Last__LastOrDefau
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            ETicaretContext context = new ETicaretContext();
+
+            ETicaretContext context = new();
+
+            #region En Temel Basit Bir Sorgulama Nasıl Yapılır?
+            #region Method Syntax
+            //var urunler = await context.Urunler.ToListAsync();
+            #endregion
+            #region Query Syntax
+            //var urunler2 = await (from urun in context.Urunler
+            //                      select urun).ToListAsync();
+            #endregion
+            #endregion
+
+            #region Sorguyu Execute Etmek İçin Ne Yapmamız Gerekmektedir?
+            #region ToListAsync 
+            #region Method Syntax
+            //var urunler = await context.Urunler.ToListAsync();
+            #endregion
+            #region Query Syntax
+            //var urunler = await (from urun in context.Urunler
+            //select urun).ToListAsync();
+            #endregion
+            #endregion
+
+            //int urunId = 5;
+            //string urunAdi = "2";
+
+            //var urunler = from urun in context.Urunler
+            //              where urun.Id > urunId && urun.UrunAdi.Contains(urunAdi)
+            //              select urun;
+
+            //urunId = 200;
+            //urunAdi = "4";
+
+            //foreach (Urun urun in urunler)
+            //{
+            //    Console.WriteLine(urun.UrunAdi);
+            //}
+
+            //await urunler.ToListAsync();
+
+            #region Foreach
+
+            //foreach (Urun urun in urunler)
+            //{
+            //    Console.WriteLine(urun.UrunAdi);
+            //}
+
+            #region Deferred Execution(Ertelenmiş Çalışma)
+            //IQueryable çalışmalarında ilgili kod yazıldığı noktada tetiklenmez/çalıştırılmaz yani ilgili kod yazıldığı noktada sorguyu generate etmez! Nerede eder? Çalıştırıldığı/execute edildiği noktada tetiklenir! İşte bu durumada ertelenmiş çalışma denir!
+            #endregion
+            #endregion
+            #endregion
+
+            #region IQueryable ve IEnumerable Nedir? Basit Olarak!
+
+            //var urunler = await (from urun in context.Urunler
+            //                     select urun).ToListAsync();
+
+            #region IQueryable
+            //Sorguya karşılık gelir.
+            //Ef core üzerinden yapılmış olan sorgunun execute edilmemiş halini ifade eder.
+            #endregion
+            #region IEnumerable
+            //Sorgunun çalıştırılıp/execute edilip verilerin in memorye yüklenmiş halini ifade eder.
+            #endregion
+            #endregion
+
+            #region Çoğul Veri Getiren Sorgulama Fonksiyonları
+            #region ToListAsync
+            //Üretilen sorguyu execute ettirmemizi sağlayan fonksiyondur.
+
+            #region Method Syntax
+            //var urunler = context.Urunler.ToListAsync();
+            #endregion
+            #region Query Syntax
+            //var urunler = (from urun in context.Urunler
+            //              select urun).ToListAsync();
+            //var urunler = from urun in context.Urunler
+            //              select urun;
+            //var datas = await urunler.ToListAsync();
+            #endregion
+            #endregion
+
+            #region Where
+            //Oluşturulan sorguya where şartı eklememizi sağlayan bir fonksiyondur.
+
+            #region Method Syntax
+            //var urunler = await context.Urunler.Where(u => u.Id > 500).ToListAsync();
+            //var urunler = await context.Urunler.Where(u => u.UrunAdi.StartsWith("a")).ToListAsync();
+            //Console.WriteLine();
+            #endregion
+            #region Query Syntax
+            //var urunler = from urun in context.Urunler
+            //              where urun.Id > 500 && urun.UrunAdi.EndsWith("7")
+            //              select urun;
+            //var data = await urunler.ToListAsync();
+            //Console.WriteLine();
+            #endregion
+            #endregion
+
+            #region OrderBy
+            //Sorgu üzerinde sıralama yapmamızı sağlayan bir fonksiyondur. (Ascending)
+
+            #region Method Syntax
+            //var urunler = context.Urunler.Where(u => u.Id > 500 || u.UrunAdi.EndsWith("2")).OrderBy(u => u.UrunAdi);
+            #endregion
+            #region Query Syntax
+            //var urunler2 = from urun in context.Urunler
+            //               where urun.Id > 500 || urun.UrunAdi.StartsWith("2")
+            //               orderby urun.UrunAdi
+            //               select urun;
+            #endregion
+
+            //await urunler.ToListAsync();
+            //await urunler2.ToListAsync();
+            #endregion
+
+            #region ThenBy
+            //OrderBy üzerinde yapılan sıralama işlemini farklı kolonlarada uygulamamızı sağlayan bir fonksiyondur. (Ascending) 
+
+            //var urunler = context.Urunler.Where(u => u.Id > 500 || u.UrunAdi.EndsWith("2")).OrderBy(u => u.UrunAdi).ThenBy(u => u.Fiyat).ThenBy(u => u.Id);
+
+            //await urunler.ToListAsync();
+            #endregion
+
+            #region OrderByDescending
+            //Descending olarak sıralama yapmamızı sağlayan bir fonksiyondur.
+
+            #region Method Syntax
+            //var urunler = await context.Urunler.OrderByDescending(u => u.Fiyat).ToListAsync();
+            #endregion
+            #region Query Syntax
+            //var urunler = await (from urun in context.Urunler
+            //                     orderby urun.UrunAdi descending
+            //                     select urun).ToListAsync();
+            #endregion
+            #endregion
+
+            #region ThenByDescending
+            //OrderByDescending üzerinde yapılan sıralama işlemini farklı kolonlarada uygulamamızı sağlayan bir fonksiyondur. (Ascending)
+            //var urunler = await context.Urunler.OrderByDescending(u => u.Id).ThenByDescending(u => u.Fiyat).ThenBy(u => u.UrunAdi).ToListAsync();
+            #endregion
+            #endregion
 
             #region Tekil Veri Getiren Sorgulama Fonksiyonları
+            //Yapılan sorguda sade ve sadece tek bir verinin gelmesi amaçlanıyorsa Single ya da SingleOrDefault fonksiyonları kullanılabilir.
+            #region SingleAsync
+            //Eğer ki, sorgu neticesinde birden fazla veri geliyorsa ya da hiç gelmiyorsa her iki durumda da exception fırlatır.
+            #region Tek Kayıt Geldiğinde
+            //var urun = await context.Urunler.SingleAsync(u => u.Id == 55);
+            #endregion
+            #region Hiç Kayıt Gelmediğinde
+            //var urun = await context.Urunler.SingleAsync(u => u.Id == 5555);
+            #endregion
+            #region Çok Kayıt Geldiğinde
+            //var urun = await context.Urunler.SingleAsync(u => u.Id > 55);
+            #endregion
+            #endregion
 
+            #region SingleOrDefaultAsync
+            //Eğer ki, sorgu neticesinde birden fazla veri geliyorsa exception fırlatır, hiç veri gelmiyorsa null döner.
+            #region Tek Kayıt Geldiğinde
+            //var urun = await context.Urunler.SingleOrDefaultAsync(u => u.Id == 55);
+            #endregion
+            #region Hiç Kayıt Gelmediğinde
+            //var urun = await context.Urunler.SingleOrDefaultAsync(u => u.Id == 5555);
+            #endregion
+            #region Çok Kayıt Geldiğinde
+            //var urun = await context.Urunler.SingleOrDefaultAsync(u => u.Id > 55);
+            #endregion
+            #endregion
+
+            //Yapılan sorguda tek bir verinin gelmesi amaçlanıyorsa First ya da FirstOrDefault fonksiyonları kullanılabilir.
+            #region FirstAsync
+            //Sorgu neticesinde elde edilen verilerden ilkini getirir. Eğer ki hiç veri gelmiyorsa hata fırlatır.
+            #region Tek Kayıt Geldiğinde
+            //var urun = await context.Urunler.FirstAsync(u => u.Id == 55);
+            #endregion
+            #region Hiç Kayıt Gelmediğinde
+            //var urun = await context.Urunler.FirstAsync(u => u.Id == 5555);
+            #endregion
+            #region Çok Kayıt Geldiğinde
+            //var urun = await context.Urunler.FirstAsync(u => u.Id > 55);
+            #endregion
+            #endregion
+
+            #region FirstOrDefaultAsync
+            //Sorgu neticesinde elde edilen verilerden ilkini getirir. Eğer ki hiç veri gelmiyorsa null değerini döndürür.
+            #region Tek Kayıt Geldiğinde
+            //var urun = await context.Urunler.FirstOrDefaultAsync(u => u.Id == 55);
+            #endregion
+            #region Hiç Kayıt Gelmediğinde
+            //var urun = await context.Urunler.FirstOrDefaultAsync(u => u.Id == 5555);
+            #endregion
+            #region Çok Kayıt Geldiğinde
+            //var urun = await context.Urunler.FirstAsync(u => u.Id > 55);
+            #endregion
+            #endregion
+
+            #region SingleAsync, SingleOrDefaultAsync, FirstAsync, FirstOrDefaultAsync Karşılaştırması
 
             #endregion
+
+            #region FindAsync
+            //Find fonksiyonu, "primary key" kolonuna özel hızlı bir şekilde sorgulama yapmamızı sağlayan bir fonksiyondur.
+            //Urun urun = await context.Urunler.FirstOrDefaultAsync(u => u.Id == 55);
+            //Urun urun = await context.Urunler.FindAsync(55);
+
+            #region Composite Primary key Durumu
+            //UrunParca u = await context.UrunParca.FindAsync(2, 5);
+            #endregion
+            #endregion
+
+            #region FindAsync İle SingleAsync, SingleOrDefaultAsync, FirstAsync, FirstOrDefaultAsync Fonksiyonlarının Karşılaştırması
+
+            #endregion
+
+            #region LastAsync
+            //Sorgu neticesinde gelen verilerden en sonuncusunu getirir. Eğer ki hiç veri gelmiyorsa hata fırlatır. OrderBy kullanılması mecburidir.
+            //var urun = await context.Urunler.OrderBy(u => u.Fiyat).LastAsync(u => u.Id > 55);
+            #endregion
+
+            #region LastOrDefaultAsync
+            //Sorgu neticesinde gelen verilerden en sonuncusunu getirir. Eğer ki hiç veri gelmiyorsa null döner. OrderBy kullanılması mecburidir.
+            //var urun = await context.Urunler.OrderBy(u => u.Fiyat).LastOrDefaultAsync(u => u.Id > 55);
+            #endregion
+            #endregion
+
+
+
         }
 
         #region Classes
+
         public class ETicaretContext : DbContext
         {
             public DbSet<Urun> Urunler { get; set; }
             public DbSet<Parca> Parcalar { get; set; }
+            public DbSet<UrunParca> UrunParca { get; set; }
 
             protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             {
+                optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ETicaretDB;User ID=SA;Password=1q2w3e4r+!");
+            }
 
-                optionsBuilder.UseSqlServer("Server=HASAN_YURDAKUL;Database=ETicaretDB;User ID=sa;Password=1q2w3e4r;TrustServerCertificate=True");
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<UrunParca>().HasKey(up => new { up.UrunId, up.ParcaId });
             }
         }
-
-        public class Parca
-        {
-            public int ParcaID { get; set; }
-            public string ParcaAdi { get; set; }
-        }
-
         public class Urun
         {
-            public int UrunID { get; set; }
+            public int Id { get; set; }
             public string UrunAdi { get; set; }
             public float Fiyat { get; set; }
+
             public ICollection<Parca> Parcalar { get; set; }
         }
+        public class Parca
+        {
+            public int Id { get; set; }
+            public string ParcaAdi { get; set; }
+        }
+        public class UrunParca
+        {
+            public int UrunId { get; set; }
+            public int ParcaId { get; set; }
+
+            public Urun Urun { get; set; }
+            public Parca Parca { get; set; }
+        }
+
+        public class UrunDetay
+        {
+            public int Id { get; set; }
+            public float Fiyat { get; set; }
+        }
         #endregion
+
     }
 }
