2f8e55506e64ba9c0b6cbeccc879092f8775dadd
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-04-13 05:13:48 +0300

    #35

---
+++ #35 - Kalıtımsal Durumlarda EF Core Davranışları TPH - TPT - TPC (EF Core 7)/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _35___Kalıtımsal_Durumlarda_EF_Core_Davranışları_TPH___TPT___TPC__EF_Core_7_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //BU DERS SUNUM UZERINDEN ISLENDI
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
